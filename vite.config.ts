e2bc93ade967f7b421fa6158ce22390321930655
donbr <dwbranson@gmail.com>
2025-02-25 03:02:51 -0800

    siimiplify vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ command }) => {
-  // For a project site, set base to the repository name
-  const base = command === 'build' ? '/donbr.github.io/' : '/'
-  
-  return {
-    plugins: [react()],
-    base,
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // base: '/',  //  Either set it to '/' OR remove this line completely.
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  }
+  },
 })
